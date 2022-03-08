89c3192686f82eeae24e50f8911284998d8d538b
Sai Manasa Yadlapalli <manasa.yadlapalli5@gmail.com>
2022-03-07 21:47:16 -0800

    [NodeJSBookApp] Updated NodeJSBookApp Assignment with correct files

--- NodeJSBookApp/NodeJSBookApp/index.js
+++ NodeJSBookApp/NodeJSBookApp/index.js
@@ -4,10 +4,13 @@ var express = require('express');
 var app = express();
 //require express middleware body-parser
 var bodyParser = require('body-parser');
+var alert = require('alert');
+// var popup = require('popups');
 //require express session
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
-
+// const popupS = require('popups');
+let error_message = '';
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 //set the directory of views
@@ -41,7 +44,7 @@ var books = [
 //route to root
 app.get('/', function (req, res) {
     //check if user session exits
-    if (req.session.user) {
+        if (req.session.user) {
         res.render('/home');
     } else
         res.render('login');
@@ -55,8 +58,13 @@ app.post('/login', function (req, res) {
         Users.filter(user => {
             if (user.username === req.body.username && user.password === req.body.password) {
                 req.session.user = user;
+                console.log("registered user");
                 res.redirect('/home');
-            }
+            } 
+            else {
+            error_message = "Incorrect Username or Password";
+            return res.render('login', { error_message: error_message } );
+        }
         });
     }
 
@@ -77,12 +85,37 @@ app.get('/create', function (req, res) {
     if (!req.session.user) {
         res.redirect('/');
     } else {
-        res.render('create');
+        error_message = "";
+        res.render('create', { error_message: error_message} );
     }
 });
 
 app.post('/create', function (req, res) {
-    // add your code
+    if (req.session.user) {
+        console.log("User Registed, need to extract info");
+        var new_book = { "BookID": req.body.book_id, "Title": req.body.title, "Author": req.body.author }
+        var isDuplicate = false;
+        for (var i=0; i<books.length; i++) {
+            if (books[i]["BookID"] == new_book["BookID"]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+
+        if (!isDuplicate) {
+            books.push(new_book)
+            res.render('home', {books: books
+            });
+        }
+
+        else {
+            error_message = "BookId is already present,Please entera Unique Id";
+            res.render('create', { error_message: error_message} );
+        }
+    // }
+    } else {
+        res.render('login');
+    }
 });
 
 app.get('/delete', function (req, res) {
@@ -90,12 +123,44 @@ app.get('/delete', function (req, res) {
     if (!req.session.user) {
         res.redirect('/');
     } else {
-        res.render('delete');
+        error_message = "";
+        res.render( './delete', {error_message: error_message} );
     }
 });
 
+app.get('/logout', function (req, res) {
+    console.log ("logged out");
+    req.session.user = false;
+    res.redirect('/');
+});
+
 app.post('/delete', function (req, res) {
-    // add your code here
+    if (req.session.user) {
+        var book_id = req.body.book_id;
+        // verify if the book exists
+        var deleted = false;
+        for (var i=0; i<books.length; i++) {
+            if (books[i]["BookID"] == book_id) {
+                books.splice(i, 1);
+                deleted = true;
+                break;
+            }
+        }
+
+        if (deleted) {
+            res.render('home', {
+                books: books
+            });
+        }
+
+        else {
+            // alert("Book id is not present");
+            error_message = "Book Id is not present";
+            res.render( 'delete', {error_message: error_message} );
+            //res.render('home', {books: books} );
+            //res.render('home',{books: books})
+        }
+    } 
 })
 
 var server = app.listen(3000, function () {
